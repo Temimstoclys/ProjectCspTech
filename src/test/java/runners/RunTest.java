d1759e52ab9460907893093592adff622b1f1b71
teminstoclys.pinheiro <ti.teminstoclys@gmail.com>
2025-05-02 13:47:47 -0300

    Melhorias e Correções no Codigo

--- src/test/java/runners/RunTest.java
+++ src/test/java/runners/RunTest.java
@@ -1,17 +1,18 @@
 package runners;
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "steps",
-        plugin = {"pretty", "html:target/cucumber-report.html"},
-        monochrome = true,
-        snippets = CucumberOptions.SnippetType.CAMELCASE
-        )
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.SNIPPET_TYPE_PROPERTY_NAME;
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
 
 public class RunTest {
 
